5bfb1f7f9beb8182cea2607d1343eeccd5092884
Logan Barkes <lbarkes@ufl.edu>
2017-06-21 11:32:07 -0400

    First Commit
    Added the initial maven project

---
+++ src/main/java/fileIndexProject/FileIndexMain.java
@@ -0,0 +1,10 @@
+package fileIndexProject;
+
+public class FileIndexMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
